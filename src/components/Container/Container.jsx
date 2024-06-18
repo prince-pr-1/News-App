013276894abef2caaabfdb26f29073232aae1fe4
prince-pr-1 <167061420+prince-pr-1@users.noreply.github.com>
2024-06-18 10:56:12 +0000

    First commit

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,10 @@
+const Container = ({ children }) => {
+  return (
+    <div className="container">
+      {children}
+      <h1>Container</h1>
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Container from "../Container/Container";
+
+const Home = () => {
+  return (
+    <>
+      <Container>
+        <h1>Homepage</h1>
+        <p>abc</p>
+        <button>click</button>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
